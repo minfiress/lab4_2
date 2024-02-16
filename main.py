4a2cbd0a45e869510acf2c67f045175e413638f5
minfiress <miinnfires@gmail.com>
2024-02-16 21:22:44 +0200

    Add function to check if a number is a power of 2

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ from utils import factorial
 
 result = factorial(5)
 print(Factorial of 5:, result)
+<<<<<<< Updated upstream
+=======
+from utils import is_power_of_5
+
+num = 25
+print(is_power_of_5(num))
+from utils import is_power_of_5
+
+num = 25
+print(is_power_of_5(num))
+>>>>>>> Stashed changes
--- utils.py
+++ utils.py
@@ -13,3 +13,10 @@ def is_prime(n):
             return False
     return True
 
+<<<<<<< Updated upstream
+=======
+def is_power_of_5(num):
+    return num > 0 and ((num & (num - 1)) == 0) and (num % 5 == 0)
+def is_power_of_5(num):
+    return num > 0 and ((num & (num - 1)) == 0) and (num % 5 == 0)
+>>>>>>> Stashed changes
